e215bc640a5b5aa0d8ef6e306fc1821bc6168fe4
deckerc1 <christian.decker@binarydefense.com>
2023-08-18 21:32:02 -0400

    new auto for go imports

--- nvim/.config/nvim/lua/core/autos.lua
+++ nvim/.config/nvim/lua/core/autos.lua
@@ -2,3 +2,12 @@ vim.api.nvim_create_autocmd(
     { "BufWritePre" },
     { callback = function() vim.lsp.buf.format(nil, 200) end, pattern = { "*" } }
 )
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        require('go.format').goimport()
+    end,
+    group = format_sync_grp,
+})
